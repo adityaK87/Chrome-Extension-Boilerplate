cadd71ddc068d1ec2aa2b2ce3da53d9465fe2a17
Aditya Kumar <adityapal1409@gmail.com>
2023-07-11 21:30:54 +0530

    modify  webpack.config.js file for tailwindcss and typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,37 @@
 module.exports = {
 	mode: "development",
 	devtool: "cheap-module-source-map",
+	module: {
+		//add
+		rules: [
+			{
+				use: "ts-loader",
+				test: /\.tsx/,
+				exclude: /node_modules/,
+			},
+			{
+				use: [
+					"style-loader",
+					"css-loader",
+					{
+						loader: "postcss-loader",
+						options: {
+							postcssOptions: {
+								indent: "postcss",
+								plugins: [tailwindcss, autoprefixer],
+							},
+						},
+					},
+				],
+				test: /\.css$/i,
+			},
+		],
+	},
+	//
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+	},
+	output: {
+		filename: "[name].js",
+	},
 };
